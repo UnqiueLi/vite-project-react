c344aef91bde80639ec4b48ffafff4fd7fd40b90
李鹏辉 <1553529191@qq.com>
2024-05-14 14:58:23 +0800

    配置@的指向路径

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// 以es模块化的方式,从node的path 模块中,导入join函数
+import { join } from 'path'
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    // 在resolve.alias对象下,配置@的指向路径
+    resolve: {
+        alias: {
+            '@':join(__dirname,'./src/')
+        }
+    }
 })
